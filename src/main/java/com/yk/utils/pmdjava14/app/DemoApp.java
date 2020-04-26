c488272aa058a5a9a4f3137fbf04a8b176250fe6
yan-khonski-it <yankhonskiy@gmail.com>
2020-04-26 12:03:03 +0200

    Initial commit

---
+++ src/main/java/com/yk/utils/pmdjava14/app/DemoApp.java
@@ -0,0 +1,30 @@
+package com.yk.utils.pmdjava14.app;
+
+import com.yk.utils.pmdjava14.app.core.SomeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// UnusedImports
+// import java.util.ArrayList;
+
+public class DemoApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoApp.class);
+
+    public static void main(final String[] args) {
+        // a - ShortVariable
+        final int aVariable = 5;
+
+        @SuppressWarnings("PMD.ShortVariable") // Here PMD ignores short name.
+        final int b = 6;
+
+        final int cResult = aVariable + b;
+        LOGGER.info("c: {}", cResult);
+
+        // SystemPrintln
+        // System.out.println("susing system out println");
+
+        // SomeClass someClass = new SomeClass(); - LocalVariableCouldBeFinal
+        final SomeClass someClass = new SomeClass();
+        LOGGER.info("someClass: {}", someClass);
+    }
+}
---
+++ src/main/java/com/yk/utils/pmdjava14/app/core/SomeClass.java
@@ -0,0 +1,5 @@
+package com.yk.utils.pmdjava14.app.core;
+
+// AbstractClassWithoutAbstractMethod
+public /* abstract */ class SomeClass {
+}
